cc568c61a5a10ea2564a8b29897cadbac528eaa8
Arsensio <100180501+Arsensio@users.noreply.github.com>
2023-03-11 16:31:34 +0600

    add GET team statistics by red and yellow card

--- src/main/java/com/example/livescore/repository/TeamStatisticsRepository.java
+++ src/main/java/com/example/livescore/repository/TeamStatisticsRepository.java
@@ -1,24 +1,45 @@
 package com.example.livescore.repository;
 
+import com.example.livescore.models.GroupEntity;
 import com.example.livescore.models.TeamStatisticsEntity;
 import com.example.livescore.models.TeamStatisticsEntityPK;
-import org.springframework.data.domain.Sort;
+import com.example.livescore.web.teamStatistics.StatisticDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 @Repository
 public interface TeamStatisticsRepository extends JpaRepository<TeamStatisticsEntity, TeamStatisticsEntityPK> {
 
+    DecimalFormat df = new DecimalFormat("0.00");
 
     @Query(value = "SELECT * FROM team_statistics WHERE group_id = ?1 AND goal_count >= 1 ORDER BY goal_count DESC", nativeQuery = true)
     List<TeamStatisticsEntity> findAllByGroupIdOrderByGoalCount(Long group);
 
     @Query(value = "SELECT * FROM team_statistics WHERE group_id = ?1 ORDER BY points DESC", nativeQuery = true)
     List<TeamStatisticsEntity> findAllByGroupIdOrderByWinCount(Long group);
+
+    @Query(value = "SELECT new com.example.livescore.web.teamStatistics.StatisticDTO(ts.id.group.groupName,t.teamName,sum(ps.redCard),ts.gamePlayed)" +
+            "FROM TeamEntity t inner join PlayerEntity p on t.teamId= p.team.teamId " +
+            "inner join PlayerStatisticsEntity ps on ps.id.player.playerId = p.playerId " +
+            "inner join TeamStatisticsEntity ts on ts.id.team.teamId = t.teamId " +
+            "WHERE ps.id.group.groupId = ?1 AND ps.redCard >=1" +
+            "group by t.teamName,ts.gamePlayed,ts.id.group.groupName " +
+            "ORDER BY sum(ps.redCard) DESC")
+    List<StatisticDTO> findAllByGroupIdOrderByRedCard(Long group);
+
+    @Query(value = "SELECT new com.example.livescore.web.teamStatistics.StatisticDTO(ts.id.group.groupName,t.teamName,sum(ps.yellowCard),ts.gamePlayed)" +
+            "FROM TeamEntity t inner join PlayerEntity p on t.teamId= p.team.teamId " +
+            "inner join PlayerStatisticsEntity ps on ps.id.player.playerId = p.playerId " +
+            "inner join TeamStatisticsEntity ts on ts.id.team.teamId = t.teamId " +
+            "WHERE ps.id.group.groupId = ?1 AND ps.yellowCard >=1" +
+            "group by t.teamName,ts.gamePlayed,ts.id.group.groupName " +
+            "ORDER BY sum(ps.yellowCard) DESC")
+    List<StatisticDTO> findAllByGroupIdOrderByYellowCard(Long group);
 
     @Modifying
     @Query("update TeamStatisticsEntity t set t.gamePlayed = t.gamePlayed + 1 where t.id = ?1")
---
+++ src/main/java/com/example/livescore/web/teamStatistics/StatisticDTO.java
@@ -0,0 +1,54 @@
+package com.example.livescore.web.teamStatistics;
+
+import com.example.livescore.enums.EventNames;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.text.DecimalFormat;
+
+import static com.example.livescore.enums.EventNames.RED_CARD;
+import static com.example.livescore.enums.EventNames.YELLOW_CARD;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class StatisticDTO {
+
+    private String statName;
+    private String groupName;
+    private String teamName;
+    private Integer statCount;
+    private Integer mathPlayed;
+
+    public StatisticDTO(String groupName, String teamName, Long redCard, Integer mathPlayed) {
+        this.groupName = groupName;
+        this.teamName = teamName;
+        this.statCount = redCard.intValue();
+        this.mathPlayed = mathPlayed;
+    }
+
+    public DistinctTeamStatisticsDTO toDistinctTeamStatisticsDTO(EventNames eventNames) {
+        DistinctTeamStatisticsDTO distinctTeamStatisticsDTO = new DistinctTeamStatisticsDTO(eventNames.getEventName(), groupName, teamName);
+
+        setStatAndPerGame(eventNames, distinctTeamStatisticsDTO);
+
+        return distinctTeamStatisticsDTO;
+    }
+
+    private void setStatAndPerGame(EventNames eventName, DistinctTeamStatisticsDTO distinctTeamStatisticsDTO) {
+        if (eventName.equals(RED_CARD)) {
+            DecimalFormat df = new DecimalFormat("0.00");
+            double redCard = this.statCount;
+            double gamePlayed = this.mathPlayed;
+            distinctTeamStatisticsDTO.setTotal(this.statCount);
+            distinctTeamStatisticsDTO.setPerGame(df.format(redCard / gamePlayed));
+        } else if (eventName.equals(YELLOW_CARD)) {
+            DecimalFormat df = new DecimalFormat("0.00");
+            double yellowCard = this.statCount;
+            double gamePlayed = this.mathPlayed;
+            distinctTeamStatisticsDTO.setTotal(this.statCount);
+            distinctTeamStatisticsDTO.setPerGame(df.format(yellowCard / gamePlayed));
+        }
+    }
+}
