8859f73e15aa2b91b38f1722572b21603551ffc3
Arsen <arsen.ulykbekov@solva.kz>
2023-03-16 15:27:54 +0600

    add find all by team_id

--- src/main/java/com/example/livescore/controllers/player/impl/DefaultPlayerController.java
+++ src/main/java/com/example/livescore/controllers/player/impl/DefaultPlayerController.java
@@ -5,8 +5,14 @@ import com.example.livescore.controllers.player.PlayerController;
 import com.example.livescore.service.player.PlayerService;
 import com.example.livescore.web.players.PlayerDTO;
 import com.example.livescore.web.players.SavePlayerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/player")
@@ -15,5 +21,11 @@ public class DefaultPlayerController extends AbstractFootballController<PlayerSe
 
     public DefaultPlayerController(PlayerService service) {
         super(service);
+    }
+
+    @GetMapping("/team/{team_id}")
+    @Override
+    public ResponseEntity<List<PlayerDTO>> allPlayerByTeamId(@PathVariable("team_id") long teamId) {
+        return new ResponseEntity<>(service.findAllByTeamId(teamId), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/livescore/service/player/PlayerService.java
+++ src/main/java/com/example/livescore/service/player/PlayerService.java
@@ -4,5 +4,8 @@ import com.example.core.service.FootballService;
 import com.example.livescore.web.players.PlayerDTO;
 import com.example.livescore.web.players.SavePlayerDTO;
 
+import java.util.List;
+
 public interface PlayerService extends FootballService<PlayerDTO, SavePlayerDTO, Long> {
+    List<PlayerDTO> findAllByTeamId(long teamId);
 }
