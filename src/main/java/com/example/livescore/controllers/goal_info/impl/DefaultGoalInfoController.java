b968b1d954b4926a6cb70addb3a7734127be14cc
Arsen <arsen.ulykbekov@solva.kz>
2023-03-20 17:51:28 +0600

    refactor goalinfo

---
+++ src/main/java/com/example/livescore/controllers/goal_info/impl/DefaultGoalInfoController.java
@@ -0,0 +1,20 @@
+package com.example.livescore.controllers.goal_info.impl;
+
+import com.example.core.controller.AbstractFootballController;
+import com.example.livescore.controllers.goal_info.GoalInfoController;
+import com.example.livescore.service.goal_info.GoalInfoService;
+import com.example.livescore.web.assists.AssistDTO;
+import com.example.livescore.web.assists.SaveAssistsDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/goal_info")
+public class DefaultGoalInfoController
+        extends AbstractFootballController<GoalInfoService, AssistDTO, SaveAssistsDTO, Long>
+        implements GoalInfoController {
+
+    public DefaultGoalInfoController(GoalInfoService service) {
+        super(service);
+    }
+}
